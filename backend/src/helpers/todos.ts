d3da3df4865900e6d7c05e2a2ac20599718a221a
Delors <stevedelors7@gmail.com>
2022-11-14 16:41:54 +0100

    added TODOs

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,28 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+// import { TodosAccess } from './todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
+// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+// import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+import { APIGatewayProxyEvent } from 'aws-lambda'
+import { getUserId } from '../lambda/utils'
+// import * as createError from 'http-errors'
 
-// TODO: Implement businessLogic
+// // TODO: Implement businessLogic
+
+export function todoCreator (todoRequest: CreateTodoRequest, event: APIGatewayProxyEvent): TodoItem {
+    const todoId = uuid.v4()
+
+    const todo = {
+      todoId: todoId,
+      userId: getUserId(event),
+      createdAt: new Date().toISOString(),
+      name: todoRequest.name,
+      dueDate: todoRequest.dueDate,
+      done: false,
+      attachmentUrl: ''
+    }
+
+    return todo as TodoItem
+}
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -3,16 +3,25 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
+import { deleteTodo, getTodoById } from '../../helpers/todosAcess'
 
-import { deleteTodo } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+//import { deleteTodo } from '../../businessLogic/todos'
+// import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Remove a TODO item by id
     
-    return undefined
+    const todo = getTodoById(todoId)
+    deleteTodo(await todo)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        "Deleted"
+      )
+    }
   }
 )
 
